6539b2f2a6732ad764a37c2fc5904accc3d8087b
Nitesh Kumar <niteshk@geekyants.com>
2023-01-18 19:24:15 +0530

    feat: added drawer ui, and code to open and close it

--- app/src/main/java/com/example/completeandroidjavaapp/MainActivity.java
+++ app/src/main/java/com/example/completeandroidjavaapp/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.completeandroidjavaapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.completeandroidjavaapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    DrawerLayout drawer;
+    ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        drawer = binding.mainDrawer;
+
+        /// we attach the current activity to the ActionBar drawer
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, R.string.nav_open, R.string.nav_close);
+
+        /// adding this, adds support for the drawer to change to back arrow when drawer is opened
+        drawer.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        /// adding this makes the drawer icon visible
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    /// adding this opens the drawer when the drawer icon is clicked
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        binding = null;
+        super.onDestroy();
     }
 }
